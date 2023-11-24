eb3a810ee2bfb7c297ca36b9e1dd3f70c3be1b93
Tony <indrajit.suryakanta.9@gmail.com>
2023-11-24 18:12:36 +0530

    cors handled 3

--- api/news.js
+++ api/news.js
@@ -1,28 +1,25 @@
 import axios from "axios";
 
 const news = async (req, res) => {
-  const { query } = req;
-  const page = query.page;
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  const options = {
-    method: "GET",
-    url: page
-      ? `https://newsapi.org/v2/everything?q=india&page=${page}&pageSize=100`
-      : `https://newsapi.org/v2/top-headlines?country=in&pageSize=100`,
-    headers: {
-      "x-api-key": "b6684226df7948ddb6df5ad210a9c9dd",
-    },
-  };
-
   try {
+    const { query } = req;
+    const page = query.page;
+    const options = {
+      method: "GET",
+      url: page
+        ? `https://newsapi.org/v2/everything?q=india&page=${page}&pageSize=100`
+        : `https://newsapi.org/v2/top-headlines?country=in&pageSize=100`,
+      headers: {
+        "x-api-key": "b6684226df7948ddb6df5ad210a9c9dd",
+      },
+    };
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     const response = await axios.request(options);
-    return response.data.articles;
+    res.json(response.data);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
 
