d5cb3e988ce71d13780fb4d854525f805d76b324
Tony <indrajit.suryakanta.9@gmail.com>
2023-11-24 17:51:54 +0530

    serverless news function added

---
+++ api/news.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const news = async (req, res) => {
+  const { query } = req;
+  const page = query.page;
+
+  const options = {
+    method: "GET",
+    url: page
+      ? `https://newsapi.org/v2/everything?q=india&page=${page}&pageSize=100`
+      : `https://newsapi.org/v2/top-headlines?country=in&pageSize=100`,
+    headers: {
+      "x-api-key": "b6684226df7948ddb6df5ad210a9c9dd",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data.articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default news;
